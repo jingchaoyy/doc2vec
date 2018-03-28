ba6cf2a6c90c239a6b12917a4c07194c62fcaf5c
jingchaoyy <jcyangt@gmail.com>
2018-03-28 15:40:41 -0400

    specify content info
    specify content info for better doc2vec model building

--- metadataBased.py
+++ metadataBased.py
@@ -1,6 +1,14 @@
+""" Analysis metadata of datasets with doc2vec,
+each metadata has been saved as a single json file,
+including dataset name, descriptions, etc.
+Author: Jingchao Yang
+Date: Mar 27 2018
+"""
+
 from os import listdir
 import json
 import gensim
+
 LabeledSentence = gensim.models.doc2vec.LabeledSentence
 from os.path import isfile, join
 
@@ -32,7 +40,7 @@ for doc in docLabels:
     else:  # if not Dataset-Metadata key, put blank to hole a position in the list
         dMeta = " "
 
-    data.append(dName + '\n' + dDesc+ '\n' + dMeta)
+    data.append('Full Name: ' + dName + '\n' + 'Description: ' + dDesc + '\n' + 'Metadata: ' + dMeta)
 # print(data[3])
 
 # for cont in data:
@@ -41,25 +49,30 @@ for doc in docLabels:
 """ Preparing the data for Gensim Doc2vec
 Gensim Doc2Vec needs model training data in an LabeledSentence iterator object
 """
+
+
 class LabeledLineSentence(object):
     # supply both the raw data and the list of labels
     def __init__(self, doc_list, labels_list):
-       self.labels_list = labels_list
-       self.doc_list = doc_list
+        self.labels_list = labels_list
+        self.doc_list = doc_list
+
     def __iter__(self):
         # loop through all the docs, and put the documents filename as a the label for each document
         for idx, doc in enumerate(self.doc_list):
-            yield LabeledSentence(words=doc.split(),tags=[self.labels_list[idx]])
+            yield LabeledSentence(words=doc.split(), tags=[self.labels_list[idx]])
+
 
 """Training the model"""
 it = LabeledLineSentence(data, docLabels)  # create the iter object
 
-model = gensim.models.Doc2Vec(vector_size=300, window=10, min_count=5, workers=11,alpha=0.025, min_alpha=0.025) # use fixed learning rate
+model = gensim.models.Doc2Vec(vector_size=300, window=10, min_count=5, workers=11, alpha=0.025,
+                              min_alpha=0.025)  # use fixed learning rate
 model.build_vocab(it)
 for epoch in range(10):
-    model.train(it,total_examples=model.corpus_count,epochs=model.epochs)
-    model.alpha -= 0.002 # decrease the learning rate
-    model.min_alpha = model.alpha # fix the learning rate, no deca
+    model.train(it, total_examples=model.corpus_count, epochs=model.epochs)
+    model.alpha -= 0.002  # decrease the learning rate
+    model.min_alpha = model.alpha  # fix the learning rate, no deca
     # model.train(it,total_examples=model.corpus_count,epochs=model.epochs)
 
 # save the model
